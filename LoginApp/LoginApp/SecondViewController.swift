e6320245997727784d5c8ef9fb38bdc5a6811076
Daniil Pavlenko <danyy.pavl@gmail.com>
2023-04-09 16:46:01 +0300

    standart code

---
+++ LoginApp/LoginApp/SecondViewController.swift
@@ -0,0 +1,12 @@
+//
+//  SecondViewController.swift
+//  LoginApp
+//
+//  Created by Даниил Павленко on 09.04.2023.
+//
+
+import UIKit
+
+final class SecondViewController: UIViewController {
+    // MARK: - Life Cylce
+}
